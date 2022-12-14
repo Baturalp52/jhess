8228c5044a0031638cf7bd5a7b015812a09505c4
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-12-14 21:35:47 +0300

    readFromFile

--- src/history/History.java
+++ src/history/History.java
@@ -1,34 +1,62 @@
 package history;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import classes.Move;
 import game.GameBoard;
 
 public class History implements HistoryFileReader {
-private GameBoard unsavedHistory;
-private ArrayList<GameBoard> GameHistory = new ArrayList<>(); 
-public void avoidHistory(Move move) {
-	
-}
+	private GameBoard unsavedHistory;
+	private ArrayList<GameHistory> history;
 
-public void clearHistory() {
-	
-}
+	public void updateHistory(Move move) {
 
-public ArrayList<GameHistory> getHistory() {
-	return null;
-} 
+	}
 
-public void readFromFile() {
-	
-}
+	public void clearHistory() throws IOException {
+		this.history.clear();
+		ObjectOutputStream oos;
 
-public void searchHistory() {
-	
-}
+		oos = new ObjectOutputStream(new FileOutputStream(new File(this.HISTORY_FILE_NAME)));
+		oos.writeObject(this.history);
+		oos.close();
 
+	}
 
+	public ArrayList<GameHistory> getHistory() {
+		return this.history;
+	}
 
+	@SuppressWarnings("unchecked")
+	public void readFromFile() {
+		File f = new File(this.HISTORY_FILE_NAME);
+		ObjectInputStream ois = null;
+
+		try {
+			ois = new ObjectInputStream(new FileInputStream(f));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			this.history = (ArrayList<GameHistory>) ois.readObject();
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void saveHistory() {
+
+	}
 
 }
