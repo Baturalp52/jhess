a7f3550158cb2aeabafcb52dfc1d3808c0e57b29
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-12-15 01:59:24 +0300

    Doğukan (#24)
    * Update Rook.java
    
    * Rook and Bishop update for reusability.
    
    * Update Queen.java
    
    Co-authored-by: Doğukan Özkaya <ozkayad7@gmail.com>

--- src/piece/Queen.java
+++ src/piece/Queen.java
@@ -1,6 +1,7 @@
 package piece;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import classes.Player;
 import enums.MOVE_TYPE;
@@ -13,8 +14,70 @@ public class Queen extends Piece {
 		// TODO Auto-generated constructor stub
 	}
 
-	public ArrayList<String> availableMoves() {
-		ArrayList<String> moves = new ArrayList<String>();
+	public HashSet<String> availableMoves() {
+		HashSet<String> moves = new HashSet<String>();
+		int row = let2Num(position.substring(0, 1));
+		int col = Integer.parseInt(position.substring(1, 2));
+		
+		int sign[] = { -1, +1 };
+		for (int rowSign : sign) {
+			for (int colSign : sign) {
+				for (int i = 1; i <= 7; i++) {
+
+					int targetRow = row + i * rowSign;
+					int targetCol = col + i * colSign;
+					if (checkPosition(targetRow, targetCol))
+						moves.add(rowColToPos(targetRow, targetCol));
+					else
+						break;
+
+				}
+			}
+		}
+		for (int i = 1; i <= 7; i++) {
+
+			int targetRow = (row + i);
+			int targetCol = col;
+			if (checkPosition(targetCol, targetCol))
+				moves.add(rowColToPos(targetRow, targetCol));
+			else
+				break;
+
+		}
+
+		for (int i = -1; i >= -7; i--) {
+			int targetRow = (row + i);
+			int targetCol = col;
+
+			if (checkPosition(targetRow, targetCol))
+				moves.add(rowColToPos(targetRow, targetCol));
+			else
+				break;
+
+		}
+		for (int i = 1; i <= 7; i++) {
+			int targetRow = (row);
+			int targetCol = col + i;
+
+			if (checkPosition(targetRow, targetCol))
+				moves.add(rowColToPos(targetRow, targetCol));
+			else
+				break;
+		}
+
+		for (int i = -1; i >= -7; i--) {
+			int targetRow = row;
+			int targetCol = col + i;
+
+			if (checkPosition(targetRow, targetCol))
+				moves.add(rowColToPos(targetRow, targetCol));
+			else
+				break;
+		}
+
+		
+		
+		
 		return moves;
 	};
 }
