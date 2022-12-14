4a9eaf29caff80a7671e550d0710dfe7a9b0d2b3
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-12-14 19:32:36 +0300

    Berra (#16)
    * Berra (#6)
    
    * initialization
    
    * test (#1)
    
    * Update Test.java (#2)
    
    Co-authored-by: Doğukan Özkaya <ozkayad7@gmail.com>
    
    * test (#3)
    
    Co-authored-by: Baturalp Sönmez <baturalpsonmez2002@gmail.com>
    
    * Doğukan (#4)
    
    * Update Test.java
    
    * Update Test.java
    
    * Doğukan (#5)
    
    * Update Test.java
    
    * Update Test.java
    
    * Update Test.java
    
    * ENUMERATION
    
    Co-authored-by: Baturalp Sönmez <baturalpsonmez2002@gmail.com>
    Co-authored-by: Doğukan Özkaya <ozkayad7@gmail.com>
    Co-authored-by: SelenKoroglu <116627326+SelenKoroglu@users.noreply.github.com>
    Co-authored-by: Doğukan Özkaya <101335869+dogukanzky@users.noreply.github.com>
    
    * update
    
    * update2
    
    Co-authored-by: berra11 <114945591+berra11@users.noreply.github.com>
    Co-authored-by: Doğukan Özkaya <ozkayad7@gmail.com>
    Co-authored-by: SelenKoroglu <116627326+SelenKoroglu@users.noreply.github.com>
    Co-authored-by: Doğukan Özkaya <101335869+dogukanzky@users.noreply.github.com>
    Co-authored-by: berra11 <berra.turgut@ug.bilkent.edu.tr>

--- src/piece/Knight.java
+++ src/piece/Knight.java
@@ -2,11 +2,16 @@ package piece;
 
 import enums.MOVE_TYPE;
 
-public abstract class Knight extends Piece {
+public  class Knight extends Piece {
 
 	public Knight(String player, String position, MOVE_TYPE moveType, String gameBoard) {
 		super(player, position, moveType, gameBoard);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public String[] availableMoves() {
+		String[] out = null;
+		return out; 
+	}; 
 
 }
--- src/piece/Pawn.java
+++ src/piece/Pawn.java
@@ -2,13 +2,16 @@ package piece;
 
 import enums.MOVE_TYPE;
 
-public abstract class Pawn extends Piece {
+public  class Pawn extends Piece {
 
 	public Pawn(String player, String position, MOVE_TYPE moveType, String gameBoard) {
 		super(player, position, moveType, gameBoard);
 		// TODO Auto-generated constructor stub
 	}
 	
-	
+	public String[] availableMoves() {
+		String[] out = null;
+		return out; 
+	}; 
 
 }
