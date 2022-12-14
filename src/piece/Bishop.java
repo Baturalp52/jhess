02ee9aa201d4e76c77fb4b6f0790ea2e999b65ac
Doğukan Özkaya <ozkayad7@gmail.com>
2022-12-15 01:47:20 +0300

    Rook and Bishop update for reusability.

--- src/piece/Bishop.java
+++ src/piece/Bishop.java
@@ -1,8 +1,9 @@
 package piece;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
 import classes.Player;
+import enums.COLOR;
 import enums.MOVE_TYPE;
 import game.GameBoard;
 
@@ -13,8 +14,41 @@ public class Bishop extends Piece {
 		// TODO Auto-generated constructor stub
 	}
 
-	public ArrayList<String> availableMoves() {
-		ArrayList<String> moves = new ArrayList<String>();
+	public HashSet<String> availableMoves() {
+		HashSet<String> moves = new HashSet<String>();
+		HashMap<String, Piece> board = gameBoard.getBoard();
+		COLOR playerColor = getPlayer().getColor();
+		int row = let2Num(position.substring(0, 1));
+		int col = Integer.parseInt(position.substring(1, 2));
+
+		int sign[] = { -1, +1 };
+		for (int rowSign : sign) {
+			for (int colSign : sign) {
+				for (int i = 1; i <= 7; i++) {
+
+					String targetRow = num2Let(row + i * rowSign);
+					int targetCol = col + i * colSign;
+					if (targetRow == null || num2Let(targetCol) == null)
+						continue;
+					String targetPos = targetRow + targetCol;
+					Piece targetPiece = board.get(targetPos);
+
+					if (targetPiece == null)
+						moves.add(targetPos);
+					else {
+						if (targetPiece.getPlayer().getColor() == playerColor)
+							continue;
+						else {
+							moves.add(targetPos);
+							continue;
+						}
+
+					}
+
+				}
+			}
+		}
+
 		return moves;
 	};
 }
