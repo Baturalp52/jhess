3837e1fdbfe056365877b68c4f6805763f492cb2
Doğukan Özkaya <ozkayad7@gmail.com>
2022-12-14 20:32:36 +0300

    Update GameHistory.java

--- src/history/GameHistory.java
+++ src/history/GameHistory.java
@@ -6,14 +6,46 @@ import classes.Move;
 import classes.Player;
 
 public class GameHistory {
-private Player[] players; 
-private String date; 
-private LinkedList<Move> moves; 
-private Player winner ;
-public GameHistory(Player[] players) {
-	super();
-	this.players = players;
-} 
+	private Player[] players;
+	private String date;
+	private LinkedList<Move> moves;
+	private Player winner;
 
+	public GameHistory(Player[] players) {
+		super();
+		this.players = players;
+	}
+
+	public Player[] getPlayers() {
+		return players;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public LinkedList<Move> getMoves() {
+		return moves;
+	}
+
+	public Player getWinner() {
+		return winner;
+	}
+
+	public void setPlayers(Player[] players) {
+		this.players = players;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public void setMoves(LinkedList<Move> moves) {
+		this.moves = moves;
+	}
+
+	public void setWinner(Player winner) {
+		this.winner = winner;
+	}
 
 }
