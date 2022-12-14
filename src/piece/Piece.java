1512bb400d4b0b31970d92cde3f4f267d0ddda95
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-12-14 23:42:05 +0300

    Static methods for Piece.java

--- src/piece/Piece.java
+++ src/piece/Piece.java
@@ -30,4 +30,50 @@ public abstract class Piece {
 		return player;
 	}
 
+	public static int let2Num(String letter) {
+		switch (letter) {
+		case "A":
+			return 1;
+		case "B":
+			return 2;
+		case "C":
+			return 3;
+		case "D":
+			return 4;
+		case "E":
+			return 5;
+		case "F":
+			return 6;
+		case "G":
+			return 7;
+		case "H":
+			return 8;
+		default:
+			return -1;
+		}
+	}
+
+	public static String num2Let(int num) {
+		switch (num) {
+		case 1:
+			return "A";
+		case 2:
+			return "B";
+		case 3:
+			return "C";
+		case 4:
+			return "D";
+		case 5:
+			return "E";
+		case 6:
+			return "F";
+		case 7:
+			return "G";
+		case 8:
+			return "H";
+		default:
+			return null;
+		}
+	}
+
 }
