5867f0209772d9aa5ba344578b90a5243c6c7a85
Doğukan Özkaya <101335869+dogukanzky@users.noreply.github.com>
2022-12-14 20:08:15 +0300

    Doğukan (#17)
    * ENUMERATION
    
    * Create Player.java
    
    * GameBoard class is created
    
    * Update GameBoard.java

--- src/game/GameBoard.java
+++ src/game/GameBoard.java
@@ -1,5 +1,43 @@
 package game;
 
+import java.util.HashMap;
+
+import classes.Player;
+import enums.MOVE_TYPE;
+import piece.Bishop;
+import piece.King;
+import piece.Knight;
+import piece.Piece;
+import piece.Queen;
+import piece.Rook;
+
 public class GameBoard {
+private Player whitePlayer;
+private Player blackPlayer;
+private HashMap<String,Piece> board;
+
+public GameBoard(Player whitePlayer, Player blackPlayer) {
+	super();
+	this.whitePlayer = whitePlayer;
+	this.blackPlayer = blackPlayer;
+	this.board= new HashMap<String,Piece>();
+	
+	
+}
+
+public Player getWhitePlayer() {
+	return whitePlayer;
+}
+
+public Player getBlackPlayer() {
+	return blackPlayer;
+}
+
+public HashMap<String, Piece> getBoard() {
+	return board;
+}
+
+
+
 
 }
