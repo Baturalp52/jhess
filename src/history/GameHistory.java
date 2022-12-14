eeab39acc4f7b173fa1ac5fceea31a05615515d4
berra11 <berra.turgut@ug.bilkent.edu.tr>
2022-12-14 20:13:17 +0300

    history

--- src/history/GameHistory.java
+++ src/history/GameHistory.java
@@ -1,5 +1,19 @@
 package history;
 
+import java.util.LinkedList;
+
+import classes.Move;
+import classes.Player;
+
 public class GameHistory {
+private Player[] players; 
+private String date; 
+private LinkedList<Move> moves; 
+private Player winner ;
+public GameHistory(Player[] players) {
+	super();
+	this.players = players;
+} 
+
 
 }
--- src/history/History.java
+++ src/history/History.java
@@ -1,5 +1,34 @@
 package history;
 
+import java.util.ArrayList;
+
+import classes.Move;
+import game.GameBoard;
+
 public class History implements HistoryFileReader {
+private GameBoard unsavedHistory;
+private ArrayList<GameBoard> GameHistory = new ArrayList<>(); 
+public void avoidHistory(Move move) {
+	
+}
+
+public void clearHistory() {
+	
+}
+
+public ArrayList<GameHistory> getHistory() {
+	return null;
+} 
+
+public void readFromFile() {
+	
+}
+
+public void searchHistory() {
+	
+}
+
+
+
 
 }
