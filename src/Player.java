f04e7bc1dab889ecf214ed4a7189519e41cbd8b0
Doğukan Özkaya <ozkayad7@gmail.com>
2022-12-14 18:41:40 +0300

    Create Player.java

---
+++ src/Player.java
@@ -0,0 +1,24 @@
+
+public class Player {
+
+	private COLOR color;
+private  String name;
+
+public Player(COLOR color, String name) {
+	super();
+	this.color = color;
+	this.name = name;
+}
+
+public COLOR getColor() {
+	return color;
+}
+
+
+
+public String getName() {
+	return name;
+}
+
+
+}
