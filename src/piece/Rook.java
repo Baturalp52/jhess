ec06052751e31c19e7c7def6fa16500015e6bc71
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-12-15 00:59:19 +0300

    Update Rook.java (#22)
    Co-authored-by: Doğukan Özkaya <ozkayad7@gmail.com>

--- src/piece/Rook.java
+++ src/piece/Rook.java
@@ -1,8 +1,10 @@
 package piece;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import classes.Player;
+import enums.COLOR;
 import enums.MOVE_TYPE;
 import game.GameBoard;
 
@@ -13,8 +15,89 @@ public class Rook extends Piece {
 		// TODO Auto-generated constructor stub
 	}
 
-	public ArrayList<String> availableMoves() {
-		ArrayList<String> moves = new ArrayList<String>();
+	public HashSet<String> availableMoves() {
+		HashSet<String> moves = new HashSet<String>();
+		HashMap<String, Piece> board = gameBoard.getBoard();
+		COLOR playerColor = getPlayer().getColor();
+		int row = let2Num(position.substring(0, 1));
+		int col = Integer.parseInt(position.substring(1, 2));
+
+		for (int i = 1; i <= 7; i++) {
+			String targetRow = num2Let(row + i);
+			int targetCol = col;
+			if (targetRow == null || num2Let(targetCol) == null)
+				break;
+			String targetPos = targetRow + targetCol;
+			Piece targetPiece = board.get(targetPos);
+
+			if (targetPiece == null)
+				moves.add(targetPos);
+			else {
+				if (targetPiece.getPlayer().getColor() == playerColor)
+					break;
+				else {
+					moves.add(targetPos);
+					break;
+				}
+
+			}
+		}
+		for (int i = -1; i >= -7; i--) {
+			String targetRow = num2Let(row + i);
+			int targetCol = col;
+			if (targetRow == null || num2Let(targetCol) == null)
+				break;
+			String targetPos = targetRow + targetCol;
+			Piece targetPiece = board.get(targetPos);
+			if (targetPiece == null)
+				moves.add(targetPos);
+			else {
+				if (targetPiece.getPlayer().getColor() == playerColor)
+					break;
+				else {
+					moves.add(targetPos);
+					break;
+				}
+			}
+		}
+		for (int i = 1; i <= 7; i++) {
+			String targetRow = num2Let(row);
+			int targetCol = col + i;
+			String targetPos = targetRow + targetCol;
+			Piece targetPiece = board.get(targetPos);
+			if (targetRow == null || num2Let(targetCol) == null)
+				break;
+			if (targetPiece == null)
+				moves.add(targetPos);
+			else {
+				if (targetPiece.getPlayer().getColor() == playerColor)
+					break;
+				else {
+					moves.add(targetPos);
+					break;
+				}
+			}
+
+		}
+		for (int i = -1; i >= -7; i--) {
+			String targetRow = num2Let(row);
+			int targetCol = col + i;
+			String targetPos = targetRow + targetCol;
+			Piece targetPiece = board.get(targetPos);
+			if (targetRow == null || num2Let(targetCol) == null)
+				break;
+			if (targetPiece == null)
+				moves.add(targetPos);
+			else {
+				if (targetPiece.getPlayer().getColor() == playerColor)
+					break;
+				else {
+					moves.add(targetPos);
+					break;
+				}
+			}
+		}
+
 		return moves;
 	};
 }
