04f1c052f509f3e9f60d0b963d1250f52fee92b8
Baturalp Sönmez <baturalpsonmez2002@gmail.com>
2022-12-14 21:56:48 +0300

    readFromFile (#20)

--- src/history/HistoryFileReader.java
+++ src/history/HistoryFileReader.java
@@ -1,7 +1,8 @@
 package history;
 
 interface HistoryFileReader {
-	public static final String FILE_NAME = "game_history.burcu";
+	public static final String HISTORY_FILE_NAME = "game_history.burcu";
+	public static final String UNSAVED_FILE_NAME = "unsaved_game.burcu";
 
 	void readFromFile();
 
